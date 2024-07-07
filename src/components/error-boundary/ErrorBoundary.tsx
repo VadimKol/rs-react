1a6eb13b8f790140a3acb0f3794a1cdd2574f5d8
Vadim Kolymbet <3t0n@inbox.ru>
2024-07-07 20:52:18 +0300

    feat: add adaptive layout to 320px

--- src/components/error-boundary/ErrorBoundary.tsx
+++ src/components/error-boundary/ErrorBoundary.tsx
@@ -1,5 +1,7 @@
 import { Component, type ErrorInfo, type ReactNode } from 'react';
 
+import error_img from '@/assets/images/loader.png';
+
 import styles from './styles.module.scss';
 import type { ErrorProps, ErrorState } from './types';
 
@@ -24,7 +26,7 @@ export class ErrorBoundary extends Component<ErrorProps, ErrorState> {
       return (
         <div className={styles.error}>
           <p className={styles.name}>{error?.name}</p>
-          <div className={styles.error_img} />
+          <img className={styles.error_img} src={error_img} alt="Rick and Morty error" width={500} height={500} />
           <p className={styles.desc}>{error?.message}</p>
           <p className={styles.desc}>Please refresh the page</p>
         </div>
