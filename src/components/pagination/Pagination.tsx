350999360181f3ce0ff2dd94dfb7771ced032aa5
Vadim Kolymbet <3t0n@inbox.ru>
2024-07-29 00:41:14 +0300

    refactor: fix DRY in pagination

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -19,35 +19,23 @@ export function Pagination({ page, total, setPage, handleClose }: PaginationProp
     paginationStyles = styles.pagination_box;
   }
 
+  const handlePage = (newPage: number): void => {
+    setPage(newPage);
+    setPageQuery({ page: String(newPage) });
+    navigate('/', { replace: true });
+  };
+
   return (
     <div className={styles.pagination} onClick={handleClose}>
       <div className={paginationStyles} onClick={handleClose}>
         {Boolean(page - 1) && (
-          <button
-            type="button"
-            aria-label="Left"
-            className={styles.pag_left}
-            onClick={() => {
-              setPage(page - 1);
-              setPageQuery({ page: String(page - 1) });
-              navigate('/', { replace: true });
-            }}
-          />
+          <button type="button" aria-label="Left" className={styles.pag_left} onClick={() => handlePage(page - 1)} />
         )}
         <div className={styles.pages}>
           <span>{page}</span>-<span>{total}</span>
         </div>
         {page < total && (
-          <button
-            type="button"
-            aria-label="Right"
-            className={styles.pag_right}
-            onClick={() => {
-              setPage(page + 1);
-              setPageQuery({ page: String(page + 1) });
-              navigate('/', { replace: true });
-            }}
-          />
+          <button type="button" aria-label="Right" className={styles.pag_right} onClick={() => handlePage(page + 1)} />
         )}
       </div>
     </div>
